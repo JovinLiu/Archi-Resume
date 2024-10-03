5e0c6143ae0248e2543bec0288f375dace8a2cf4
Jovin Liu <liujovin@gmail.com>
2024-10-04 00:07:40 +1000

    20241004

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Sidebar from "./Layout/Sidebar";
 
 function App() {
   return (
-    <main className="flex h-[297mm] flex-row justify-center bg-neutral-500">
+    <main className="flex flex-row justify-center">
       <div className="bg-black-400 flex h-[297mm] w-[210mm] flex-row">
         <Sidebar />
         <Experience />

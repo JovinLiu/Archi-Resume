a7924ecff0ccec7599cd78c2c56fa55c7eb57e0d
Jovin Liu <liujovin@gmail.com>
2024-10-04 00:41:09 +1000

    20241004

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import Experience from "./Layout/Experience";
 import Sidebar from "./Layout/Sidebar";
+import Column from "./UI/Column";
 
 function App() {
+  function handleClick() {
+    const link = document.createElement("a");
+    link.href = "/Resume-Jovin-Liu-Full-Stack.pdf";
+    link.download = "Resume-Jovin-Liu-Full-Stack.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
   return (
-    <main className="flex flex-row justify-center">
-      <div className="bg-black-400 flex h-[297mm] w-[210mm] flex-row">
-        <Sidebar />
-        <Experience />
-      </div>
-    </main>
+    <Column>
+      <button
+        className="h-10 bg-sky-200 transition-all hover:bg-sky-500 hover:text-white"
+        onClick={handleClick}
+      >
+        DOWNLOAD RESUME
+      </button>
+      <main className="flex flex-row justify-center">
+        <div className="bg-black-400 flex h-[297mm] w-[210mm] flex-row">
+          <Sidebar />
+          <Experience />
+        </div>
+      </main>
+    </Column>
   );
 }
 

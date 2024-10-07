e911b11f3d0283fb672c5f4140b3525eb6c67ca4
Jovin Liu <liujovin@gmail.com>
2024-10-07 12:30:06 +1100

    20241007

--- src/Components/NameAndTitle.jsx
+++ src/Components/NameAndTitle.jsx
@@ -3,10 +3,9 @@ import Title from "../UI/Title";
 
 function NameAndTitle() {
   return (
-    // <Column className="bg-gradient-to-b from-indigo-800 to-indigo-400 bg-clip-text">
-    <Column>
+    <Column className="bg-gradient-to-b from-indigo-800 to-indigo-400 bg-clip-text">
       <Title type="master1" className="text-transparent">
-        <strong>Jovin Liu</strong>
+        <strong>Jovin Liu</strong>g
       </Title>
       <Title type="master2" className="text-transparent">
         <strong>Full Stack Web Developer</strong>
